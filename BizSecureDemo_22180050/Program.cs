f6f525b5b2b210ab80732c019e3faeb1a26e94a8
bkremenarova_22180050@unwe.bg <bkremenarova_22180050@unwe.bg>
2026-03-04 14:32:44 +0200

    Commit changes after Seminar 4- BurpSuite - Brute force attack

--- BizSecureDemo_22180050/Program.cs
+++ BizSecureDemo_22180050/Program.cs
@@ -1,48 +1,105 @@
+//using BizSecureDemo_22180050.Data;
+//using BizSecureDemo_22180050.Models;
+//using BizSecureDemo_22180050.Data;
+//using BizSecureDemo_22180050.Models;
+//using Microsoft.AspNetCore.Authentication.Cookies;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllersWithViews();
+
+//builder.Services.AddDbContext<AppDbContext>(opt =>
+//    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+//builder.Services.AddSingleton<PasswordHasher<AppUser>>();
+
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(o =>
+//    {
+//        o.LoginPath = "/Account/Login";
+//        o.LogoutPath = "/Account/Logout";
+//        o.Cookie.HttpOnly = true;
+//        o.Cookie.SameSite = SameSiteMode.Lax;
+//        o.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+//    });
+
+//builder.Services.AddAuthorization();
+//var app = builder.Build();
+//// създаваме DB автоматично (без миграции)
+////using (var scope = app.Services.CreateScope())
+////{
+////    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+////    db.Database.EnsureCreated();
+////}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    db.Database.Migrate();
+//}
+//app.UseStaticFiles();
+//app.UseRouting();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.Run();
+
+////app.UseDeveloperExceptionPage();.
+///
 using BizSecureDemo_22180050.Data;
 using BizSecureDemo_22180050.Models;
 using BizSecureDemo_22180050.Data;
 using BizSecureDemo_22180050.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-
+ opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddSingleton<PasswordHasher<AppUser>>();
-
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(o =>
-    {
-        o.LoginPath = "/Account/Login";
-        o.LogoutPath = "/Account/Logout";
-        o.Cookie.HttpOnly = true;
-        o.Cookie.SameSite = SameSiteMode.Lax;
-        o.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-    });
-
+ .AddCookie(o =>
+ {
+     o.LoginPath = "/Account/Login";
+     o.LogoutPath = "/Account/Logout";
+     o.Cookie.HttpOnly = true;
+     o.Cookie.SameSite = SameSiteMode.Lax;
+     o.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+ });
 builder.Services.AddAuthorization();
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("login", o =>
+    {
+        o.PermitLimit = 5; // 5 attempts
+        o.Window = TimeSpan.FromMinutes(1);
+        o.QueueLimit = 0;
+    });
+});
 var app = builder.Build();
-// създаваме DB автоматично (без миграции)
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
-
 app.UseStaticFiles();
 app.UseRouting();
-
+app.UseRateLimiter();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+ name: "default",
+ pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+ name: "login",
+ pattern: "Account/Login",
+ defaults: new { controller = "Account", action = "Login" })
+ .RequireRateLimiting("login");
 app.Run();
-
-//app.UseDeveloperExceptionPage();.
