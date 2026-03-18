8e0870dae44106d2b5e45a5a59366079657a26a3
bkremenarova_22180050@unwe.bg <bkremenarova_22180050@unwe.bg>
2026-03-18 11:45:11 +0200

    Changes made with the Replay Attack

---
+++ BizSecureDemo_22180050/ViewModels/ReplayDemoVm.cs
@@ -0,0 +1,14 @@
+﻿namespace BizSecureDemo_22180050.ViewModels
+{
+    public class ReplayDemoVm
+    {
+        public decimal Balance { get; set; }
+        public string? Message { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; } = 100;
+        public string Token { get; set; } = "SECRET123";
+        public string Nonce { get; set; } = string.Empty;
+        
+
+    }
+}
